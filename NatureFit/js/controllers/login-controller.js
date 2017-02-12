1768c2d64effd13aba5274b9376d6578fbaafda6
Andrew Balberchak <abalberchak@gmail.com>
2017-02-12 12:56:54 -0800

    michael changes

--- NatureFit/js/controllers/login-controller.js
+++ NatureFit/js/controllers/login-controller.js
@@ -6,15 +6,7 @@ myApp.controller('LoginController', function($scope, CloverFactory, $window) {
     $window.location.href="index.html#/calendar";
   }
   $scope.signup = function() {
-    CloverFactory.addNewCustomer($scope.newUser.then(function successCallback(response) {
-        alert("Congrats, " + $scope.newUser.fullName.split(" ")[0] + ". Please sign in.");
-        $scope.newUser = null;
-        delete $scope.newUser;
-      console.log(response);
-      return response;
-    }, function errorCallback(response) {
-      console.log(response);
-    }));
+    CloverFactory.addNewCustomer($scope.newUser);
     delete $scope.newUser;
   }
 });
